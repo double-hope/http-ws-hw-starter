41974f29999142caffafa6a8d86b2f44a4d6f265
double-hope <nadia.prohorchuk@gmail.com>
2022-07-23 22:02:27 +0300

    ONO RABOTAET

--- public/javascript/game.mjs
+++ public/javascript/game.mjs
@@ -1,7 +1,46 @@
+import {showInputModal} from './views/modal.mjs';
+import {appendRoomElement} from './views/room.mjs';
+
+let _rooms = [];
+
 const username = sessionStorage.getItem('username');
+const createRoom = document.getElementById('add-room-btn');
+let _name = '';
+
+const socket = io('', { query: { username } });
 
 if (!username) {
 	window.location.replace('/login');
 }
 
-const socket = io('', { query: { username } });
+createRoom.addEventListener('click', ()=>{
+	showInputModal({title: 'Create room', onChange: roomName, onSubmit: submit})
+});
+
+const roomName = (name) =>{
+	_name = name;
+}
+
+const submit = () =>{
+	if(!_name){
+		alert('Enter room name');
+		return;
+	}
+	const room = {
+		name: _name,
+		numberOfUsers: 1,
+	}
+	socket.emit('CREATE_ROOM', room);
+}
+
+const updateRooms = rooms => {
+	_rooms = rooms;
+	_rooms.map(room => appendRoomElement({name: room.name, numberOfUsers: room.numberOfUsers}));
+}
+
+const updateCreated = room => {
+	appendRoomElement({name: room.name, numberOfUsers: room.numberOfUsers});
+}
+
+socket.on('UPDATE_CREATED_ROOMS', updateCreated);
+socket.on('UPDATE_ROOMS', updateRooms);
--- src/socket/index.ts
+++ src/socket/index.ts
@@ -1,8 +1,18 @@
 import { Server } from 'socket.io';
 import * as config from './config';
 
+let rooms = [];
+
 export default (io: Server) => {
 	io.on('connection', socket => {
 		const username = socket.handshake.query.username;
+
+		socket.emit('UPDATE_ROOMS', rooms);
+
+		socket.on('CREATE_ROOM', room => {
+			rooms = rooms.concat(room);
+			socket.emit('UPDATE_CREATED_ROOMS', room);
+			socket.broadcast.emit('UPDATE_CREATED_ROOMS', room);
+		})
 	});
 };
